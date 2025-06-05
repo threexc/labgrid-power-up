ab24556aac88143bfb25089df0001c65cd959a6c
Trevor Gamblin <tgamblin@baylibre.com>
2025-06-05 15:07:11 -0400

    basic_setup: add strategy draft
    This doesn't quite work yet, but it can be used for testing and further
    development.
    
    Signed-off-by: Trevor Gamblin <tgamblin@baylibre.com>

---
+++ basic_setup/beagleplaybootstrategy.py
@@ -0,0 +1,96 @@
+import enum
+
+import attr
+
+from labgrid.factory import target_factory
+from labgrid.strategy.common import Strategy, StrategyError
+
+
+class Status(enum.Enum):
+    unknown = 0
+    off = 1
+    uboot = 2
+    shell = 3
+
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class BeagleplayBootStrategy(Strategy):
+    """BeagleplayBootStrategy - Strategy to switch to uboot or shell"""
+    bindings = {
+        "power": "PowerProtocol",
+        "console": "ConsoleProtocol",
+        "uboot": "UBootDriver",
+        "shell": "ShellDriver",
+        "tftp": "TFTPProviderDriver"
+    }
+
+    status = attr.ib(default=Status.unknown)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.staged = False
+
+    def _stage(self):
+        if self.staged:
+            return
+        self.target.activate(self.tftp)
+
+        for name, image in self.target.env.config.get_images().items():
+            if name.startswith('tftp-'):
+                self.tftp.stage(image)
+
+        self.target.deactivate(self.tftp)   
+        self.staged = True
+
+    def _set_server_ip(self):
+        serverip = "192.168.40.134" #FIXME
+        tftpdir = self.tftp.get_export_vars()['internal']
+        
+        self.uboot.run(f"setenv autoload no; dhcp")
+        self.uboot.run(f"setenv serverip {serverip}")
+        self.uboot.run(f"setenv tftpdir {tftpdir}")
+
+    def transition(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.unknown:
+            raise StrategyError(f"can not transition to {status}")
+        elif status == self.status:
+            return # nothing to do
+        elif status == Status.off:
+            self.target.deactivate(self.console)
+            self.target.activate(self.power)
+            self.power.off()
+        elif status == Status.uboot:
+            self.transition(Status.off)
+            self.target.activate(self.console)
+            # cycle power
+            self.power.cycle()
+            # interrupt uboot
+            self.target.activate(self.uboot)
+            self._stage()
+        elif status == Status.shell:
+            # transition to uboot
+            self.transition(Status.uboot)
+            self._set_server_ip()
+            self.uboot.boot("tftp")
+            self.uboot.await_boot()
+            self.target.activate(self.shell)
+        else:
+            raise StrategyError(f"no transition found from {self.status} to {status}")
+        self.status = status
+
+    def force(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.off:
+            self.target.activate(self.power)
+        elif status == Status.uboot:
+            self.target.activate(self.uboot)
+        elif status == Status.shell:
+            self.target.activate(self.shell)
+        else:
+            raise StrategyError("can not force state {}".format(status))
+        self.status = status
+
