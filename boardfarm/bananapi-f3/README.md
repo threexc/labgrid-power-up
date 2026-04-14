562e9987dc6c4c51bb5916ecf25cebd7d3d00d63
Trevor Gamblin <tgamblin@ecocode.ca>
2026-04-14 13:40:17 -0400

    boardfarm/bananapi-f3: add
    Base the labgrid files on the Muse Pi Pro ones, but instead of using the nfsroot
    (which is meant for the OrangePi RV2), boot the rootfs from the eMMC in the
    tftpboot case. Also leave the 'emmc' boot option in the client.yaml, but leave
    it out of the pytest options for now.
    
    Signed-off-by: Trevor Gamblin <tgamblin@ecocode.ca>

---
+++ boardfarm/bananapi-f3/README.md
@@ -0,0 +1,23 @@
+# muse-pi-pro
+
+## Usage
+
+### export
+
+`labgrid-exporter boardfarm/muse-pi-pro/exporter.yaml`
+
+### create a place
+
+`labgrid-client -p bf-muse-pi-pro create`
+
+### add the resource match
+
+`labgrid-client -p bf-muse-pi-pro add-match */muse-pi-pro-setup-1/*/*`
+
+### acquire the place
+
+`labgrid-client -p bf-muse-pi-pro acquire`
+
+### boot it to the default shell (via uboot)
+
+`labgrid-client -c boardfarm/muse-pi-pro/client.yaml -s emmc con`
---
+++ boardfarm/bananapi-f3/strategy.py
@@ -0,0 +1,129 @@
+import enum
+
+import attr
+import time
+
+from labgrid.factory import target_factory
+from labgrid.strategy.common import Strategy, StrategyError
+
+
+class Status(enum.Enum):
+    unknown = 0
+    off = 1
+    uboot = 2
+    emmc = 3
+    tftp = 4
+
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class BananaPiF3BootStrategy(Strategy):
+    """BananaPiF3BootStrategy - Strategy to switch to uboot or shell"""
+    bindings = {
+        "power": "PowerProtocol",
+        "console": "ConsoleProtocol",
+        "uboot": "UBootDriver",
+        "shell": "ShellDriver",
+        "tftp": "TFTPProviderDriver",
+    }
+
+    status = attr.ib(default=Status.unknown)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.staged = False
+
+    def _stage(self):
+        if self.staged:
+            return
+        self.target.activate(self.tftp)
+
+        for name, image in self.target.env.config.get_images().items():
+            if name.startswith('tftp-'):
+                self.tftp.stage(image)
+
+        self.target.deactivate(self.tftp)   
+        self.staged = True
+
+    def _set_server_ip(self):
+        serverip = "192.168.40.134"
+        tftpdir = self.tftp.get_export_vars()['internal']
+        self.uboot.run(f"setenv autoload no")
+        try:
+            self.uboot.run(f"dhcp", timeout=10)
+        except Exception as e:
+            self.uboot.run(f"dhcp")
+        
+        self.uboot.run(f"setenv serverip {serverip}")
+
+    def _set_bootargs(self):
+        bootargs = (
+            "root=PARTUUID=1e270826-01 "
+            "earlycon=sbi "
+            "console=tty1 "
+            "console=ttyS0,115200n8 "
+            "loglevel=7 "
+            "rw "
+            "rootwait "
+            "rootfstype=ext4 "
+        )
+        self.uboot.run(f"setenv bootargs {bootargs}")
+        self.uboot.run(f" echo $bootargs")
+
+    def _get_tftp_files(self):
+        self.uboot.run(f"tftpboot $kernel_addr_r bananapi-f3/Image")
+        self.uboot.run(f"tftpboot $dtb_addr bananapi-f3/k1-bananapi-f3.dtb")
+
+    def transition(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.unknown:
+            raise StrategyError(f"can not transition to {status}")
+        elif status == self.status:
+            return # nothing to do
+        elif status == Status.off:
+            self.target.deactivate(self.console)
+            self.target.activate(self.power)
+            self.power.off()
+        elif status == Status.uboot:
+            self.transition(Status.off)
+            self.target.activate(self.console)
+            # cycle power
+            self.power.cycle()
+            # interrupt uboot
+            self.target.activate(self.uboot)
+            self._stage()
+        elif status == Status.emmc:
+            # transition to uboot
+            self.transition(Status.uboot)
+            self.uboot.boot("emmc")
+            self.uboot.await_boot()
+            self.target.activate(self.shell)
+        elif status == Status.tftp:
+            # transition to uboot
+            self.transition(Status.uboot)
+            self._set_server_ip()
+            self._set_bootargs()
+            self._get_tftp_files()
+            self.uboot.boot("tftp")
+            self.uboot.await_boot()
+            self.target.activate(self.shell)
+        else:
+            raise StrategyError(f"no transition found from {self.status} to {status}")
+        self.status = status
+
+    def force(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.off:
+            self.target.activate(self.power)
+        elif status == Status.uboot:
+            self.target.activate(self.uboot)
+        elif status == Status.emmc:
+            self.target.activate(self.shell)
+        elif status == Status.tftp:
+            self.target.activate(self.shell)
+        else:
+            raise StrategyError("can not force state {}".format(status))
+        self.status = status
+
